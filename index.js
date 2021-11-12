2515709942d7a89d1abf32955189209b6457c55d
mhasancy <mhasancy@gmail.com>
2021-11-12 16:47:44 +0600

    added env and put, post

--- index.js
+++ index.js
@@ -1,10 +1,158 @@
+//imported and express file
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Niche server running successfully.");
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//database user-password connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.86fnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+//data async await function
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("greenWatchMaster");
+    const productsCollection = database.collection("products");
+    const ordersCollection = database.collection("orders");
+    const blogsCollection = database.collection("blogs");
+    const teamCollection = database.collection("team");
+    const reviewsCollection = database.collection("reviews");
+    const usersCollection = database.collection("users");
+
+    //products data load
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    //users data load
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+    //reviews data load
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+    //team data load
+    app.get("/team", async (req, res) => {
+      const cursor = teamCollection.find({});
+      const team = await cursor.toArray();
+      res.send(team);
+    });
+    //blogs data load
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogsCollection.find({});
+      const blogs = await cursor.toArray();
+      res.send(blogs);
+    });
+    //orders data load
+    app.get("/orders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    //products data adding
+    app.post("/products", async (req, res) => {
+      const service = req.body;
+      const result = await productsCollection.insertOne(service);
+      res.json(result);
+    });
+    //users data adding
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+    //users data adding
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+    //orders data adding
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.json(result);
+    });
+    //products data adding
+    app.post("/products", async (req, res) => {
+      const products = req.body;
+      const result = await productsCollection.insertOne(products);
+      res.json(result);
+    });
+    //reviews data adding
+    app.post("/reviews", async (req, res) => {
+      const reviews = req.body;
+      const result = await reviewsCollection.insertOne(reviews);
+      res.json(result);
+    });
+
+    //orders data deleting
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+    //products data deleting
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.json(result);
+    });
+    //status updating from  pending to approved
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const requestedStatus = req.body;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const statusChange = {
+        $set: {
+          status: requestedStatus.status,
+        },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        statusChange,
+        option
+      );
+      res.json(result);
+    });
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+//data testing if server is running
+app.get("/", (req, res) => {
+  res.send("Niche server is running.");
+});
+//server is running and showing server console
 app.listen(port, () => {
-  console.log("Listening to port ", port);
+  console.log("Running on port ", port);
 });
